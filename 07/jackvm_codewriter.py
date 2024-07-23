d176281c10168cd597ce15f80ccd9673cd86e245
Kenneth Bruce <kennebru@amazon.com>
2024-07-23 09:53:45 -0400

    using function calls for popd/pushd for consistency

--- 07/jackvm_codewriter.py
+++ 07/jackvm_codewriter.py
@@ -38,47 +38,45 @@ def _drop():
     return f"@SP\nM=M-1"
 
 def _push(segment, index):
-    pushd=_pushd()
     index = int(index)
     if segment == "argument":
-        return f"@{index}\nD=A\n@ARG\nA=D+M\nD=M\n{pushd}"
+        return f"@{index}\nD=A\n@ARG\nA=D+M\nD=M\n{_pushd()}"
     elif segment == "local":
-        return f"@{index}\nD=A\n@LCL\nA=D+M\nD=M\n{pushd}"
+        return f"@{index}\nD=A\n@LCL\nA=D+M\nD=M\n{_pushd()}"
     elif segment == "static":
-        return f"@static.{index}\nD=M\n{pushd}"
+        return f"@static.{index}\nD=M\n{_pushd()}"
     elif segment == "constant":
-        return f"@{index}\nD=A\n{pushd}"
+        return f"@{index}\nD=A\n{_pushd()}"
     elif segment == "this":
-        return f"@{index}\nD=A\n@THIS\nA=D+M\nD=M\n{pushd}"
+        return f"@{index}\nD=A\n@THIS\nA=D+M\nD=M\n{_pushd()}"
     elif segment == "that":
-        return f"@{index}\nD=A\n@THAT\nA=D+M\nD=M\n{pushd}"
+        return f"@{index}\nD=A\n@THAT\nA=D+M\nD=M\n{_pushd()}"
     elif segment == "pointer":
         assert index in [0,1]
-        return f"@{index+3}\nD=M\n{pushd}"
+        return f"@{index+3}\nD=M\n{_pushd()}"
     elif segment == "temp":
         assert index in list(range(8))
-        return f"@{index+5}\nD=M\n{pushd}"
+        return f"@{index+5}\nD=M\n{_pushd()}"
     else:
         raise Exception(f"Unhandled segment: {segment}")
 
 def _pop(segment, index):
-    popd=_popd()
     index = int(index)
     if segment == "argument":
-        return f"@{index}\nD=A\n@ARG\nM=D+M\n{popd}\n@ARG\nA=M\nM=D\n@{index}\nD=A\n@ARG\nM=M-D"
+        return f"@{index}\nD=A\n@ARG\nM=D+M\n{_popd()}\n@ARG\nA=M\nM=D\n@{index}\nD=A\n@ARG\nM=M-D"
     elif segment == "local":
-        return f"@{index}\nD=A\n@LCL\nM=D+M\n{popd}\n@LCL\nA=M\nM=D\n@{index}\nD=A\n@LCL\nM=M-D"
+        return f"@{index}\nD=A\n@LCL\nM=D+M\n{_popd()}\n@LCL\nA=M\nM=D\n@{index}\nD=A\n@LCL\nM=M-D"
     elif segment == "static":
-        return f"{popd}\n@static.{index}\nM=D"
+        return f"{_popd()}\n@static.{index}\nM=D"
     elif segment == "this":
-        return f"@{index}\nD=A\n@THIS\nM=D+M\n{popd}\n@THIS\nA=M\nM=D\n@{index}\nD=A\n@THIS\nM=M-D"
+        return f"@{index}\nD=A\n@THIS\nM=D+M\n{_popd()}\n@THIS\nA=M\nM=D\n@{index}\nD=A\n@THIS\nM=M-D"
     elif segment == "that":
-        return f"@{index}\nD=A\n@THAT\nM=D+M\n{popd}\n@THAT\nA=M\nM=D\n@{index}\nD=A\n@THAT\nM=M-D"
+        return f"@{index}\nD=A\n@THAT\nM=D+M\n{_popd()}\n@THAT\nA=M\nM=D\n@{index}\nD=A\n@THAT\nM=M-D"
     elif segment == "pointer":
         assert index in [0,1]
-        return f"{popd}\n@{index+3}\nM=D"
+        return f"{_popd()}\n@{index+3}\nM=D"
     elif segment == "temp":
         assert index in list(range(8))
-        return f"{popd}\n@{index+5}\nM=D"
+        return f"{_popd()}\n@{index+5}\nM=D"
     else:
         raise Exception(f"Unhandled segment: {segment}")
