6a0a4f14b74cf3182fbf4395599a2c1fee4f0230
Kenneth Bruce <me@kenneth-bruce.com>
2024-07-22 22:08:31 -0400

    cleanup project 7

--- 07/jacktranslator.py
+++ 07/jacktranslator.py
@@ -15,44 +15,37 @@ def end_code():
 # Assemble the current instruction for the parser
 def write_cur_instr(parser,deconflicter):
     print(f"//{str(parser)}")
-    if parser.instruction_type()==VMParser.C_ARITHMETIC:
+    i_type=parser.instruction_type()
+    if i_type==VMParser.C_ARITHMETIC:
         print(f"{code.writeArithmetic(parser.arg1(), deconflicter)}")
-    elif parser.instruction_type() in [VMParser.C_PUSH, VMParser.C_POP]:
-        print(f"{code.writePushPop(parser.instruction_type(), parser.arg1(), parser.arg2())}")
+    elif i_type in [VMParser.C_PUSH, VMParser.C_POP]:
+        print(f"{code.writePushPop(i_type, parser.arg1(), parser.arg2())}")
     else:
         raise Exception(f"Unsupported instruction type: {parser.instruction_type()}")
 
-def output_assembly(filename, debug=False):
-    translate = debug_output_write if debug else write_cur_instr
+def output_assembly(filename):
+    translate = write_cur_instr
     parser=VMParser(filename)
     deconflicter=0
-    assembly_result=translate(parser,deconflicter)
+    assembly_result=translate(parser, deconflicter)
     if assembly_result: print(assembly_result)
     while parser.has_more_lines():
         deconflicter += 1
         parser.advance()
-        assembly_result=translate(parser,deconflicter)
+        assembly_result=translate(parser, deconflicter)
         if assembly_result: print(assembly_result)
 
-def debug_output_assemble(parser, deconflicter):
-    if parser.instruction_type()==VMParser.C_ARITHMETIC:
-        pass
-    else:
-        raise Exception(f"Unsupported instruction type: {parser.instruction_type()}")
-
-def do_translate(filename,debug=False):
+def do_translate(filename):
     print(init_code())
-    output_assembly(filename, debug)
+    output_assembly(filename)
     print(end_code())
     return
 
-usage_message=f"Usage: ./{sys.argv[0]} [filename.vm] [--debug]"
+usage_message=f"Usage: ./{sys.argv[0]} [filename.vm]"
 if len(sys.argv)<2:
     print(usage_message)
 elif len(sys.argv)==2:
     do_translate(sys.argv[1])
-elif len(sys.argv)==3 and sys.argv[2]=='--debug':
-    do_translate(sys.argv[1],debug=True)
 else:
     print(usage_message)
 
