8e51e788c066506c719efeb0422e4112d23fb555
Kenneth Bruce <me@kenneth-bruce.com>
2024-09-17 10:31:44 -0400

    read file from argument using argp

--- 10/main.c
+++ 10/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
 #include "llist.h"
+
+const char *argp_program_version = "jack-compy 0.1";
+const char *argp_program_bug_address = "<me@kenneth-bruce.com>";
+
+static char doc[] =
+	"jack-compy -- compile jack code to hack asm";
+
+static char args_doc[] =
+	"TARGET_FILE";
+
+static struct argp_option options[] = {
+	{"file", 'f', "FILE", 0, "File to compile"},
+	{0}
+};
+
+struct arguments
+{
+	char *file;
+};
+
+static error_t parse_opt (int key, char* arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+
+	/*printf("key %d\n", key);*/
+
+	switch (key) {
+		case 'f':
+			/*printf("arg_num %d arg %s\n", state->arg_num, arg);*/
+			arguments->file = arg;
+			break;
+		case ARGP_KEY_ARG:
+			if (state->arg_num > 2) {
+				/*printf("too many args arg_num %d\n", state->arg_num);*/
+				argp_usage (state);
+			}
+			//may need to put this on arguments?
+			break;
+		/*case ARGP_KEY_END: //Copied this from example but it doesn't seem to be correct
+			if (state->arg_num < 1) {
+				printf("not enough args arg_num %d\n", state->arg_num);
+				argp_usage(state);
+			}
+			break;*/
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	/*printf("finished switch\n");*/
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char**argv) {
 
@@ -9,8 +61,12 @@ int main(int argc, char**argv) {
 	size_t len = 0;
 	ssize_t read;
 	sllist* lines = sll_create();
+	struct arguments arguments;
+	arguments.file = "./main.c";
 
-	fp = fopen("./main.c", "r");
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	fp = fopen(arguments.file, "r");
 	if (fp==NULL) {
 		return 1;
 	}
