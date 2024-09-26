489a8528b9807e680e557ae6a66f81aa594bfcca
Kenneth Bruce <me@kenneth-bruce.com>
2024-09-26 16:28:10 -0400

    add rktest for testing

--- 10/test.c
+++ 10/test.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// rktest
+#define DEFINE_RKTEST_IMPLEMENTATION
+#include "rktest/rktest.h"
+
 // Project Includes
 #include "llist.h"
 #include "tokenizer.h"
 
 
-int main(int argc, char**argv) {
-	printf("===\nBEGIN TEST OUTPUT\n===");
+int main(int argc, const char**argv) {
+	return rktest_main(argc, argv);
+}
 
+TEST(token_tests, can_init) {
 	init_tokenizer();
-
-	return 0;
+	EXPECT_TRUE(1);
 }
