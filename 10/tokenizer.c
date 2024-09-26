f312bb40c824bb44108461c7d6b10199729143fe
Kenneth Bruce <me@kenneth-bruce.com>
2024-09-25 21:09:38 -0400

    tokenizer: advance logic implemented, all code UNTESTED

--- 10/tokenizer.c
+++ 10/tokenizer.c
@@ -8,6 +8,7 @@ typedef struct codellist {
 } codelist;
 
 enum TOKEN_TYPE {
+	T_NULL,
 	T_KEYWORD,
 	T_SYMBOL,
 	T_IDENTIFIER,
@@ -51,26 +52,116 @@ static char cur_token[512] = "";
 int hasMoreTokens(codelist *c) {
 	return (c->pos || c->line);
 }
+
+int char_is_whitespace(char c) {
+	return ((c == '\n') || (c == ' ') || (c == '\t'));
+}
+
+int char_is_int(char c) {
+	return (c >= '0' && c <= '9');
+}
+
+int char_is_symbol(char c) {
+	for (int i=0;i<SYM_COUNT;i++) {
+		if (SYMBOLS[i]==c)
+			return 1;
+	}
+	return 0;
+}
+
+int char_is_alphabet(char c) {
+	return (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')));
+}
+
+
 /* Advance to the next token */
 void advance(codelist *c) {
 	STR_LITERAL = 0;
-	
+	cur_token[0] = NULL;
+
+
+	// Move past any whitespace
+	while (char_is_whitespace(c->pos)) {
+		c->pos++;
+		if (c->pos == NULL) {
+			if (hasMoreTokens(c)) {
+				c->line = c->line->next;
+				c->pos = c->line->field[0]
+			} else {
+				return;
+			}
+		}
+	}
+
+	// Symbols are 1 char
+	if (char_is_symbol(*(c->pos))){
+		cur_token[0] = *(c->pos);
+		cur_token[1] = NULL;
+		c->pos++;
+		return;
+	}
+
+	// Ints are uninterupted string of ints
+	if (char_is_int(*(c->pos))){
+		int i=0
+		do {
+			cur_token[i] = *(c->pos);
+			i++;
+			c->pos++;
+		} while(char_is_int(*(c->pos)))
+		cur_token[i]=NULL;
+		return;
+	}
+
+	// Quoted string
+	if (*(c->pos) == '"') {
+		c->pos++;
+		int i = 0;
+		while (*(c->pos) != '"') {
+			cur_token[i] = *(c->pos);
+			i++;
+			c->pos++;
+		}
+		c->pos++; //move past closing '"'
+		cur_token[i]=NULL;
+		STR_LITERAL = 1; // Flag set
+		return
+	}
+
+	// Otherwise it should be either a keyword or an identifier, either is copied literally
+	if (char_is_alphabet(*(c->pos)) || (*(c->pos) == '_')) {
+		int i = 0;
+		while (char_is_alphabet(*(c->pos)) || (*(c->pos) == '_') || char_is_int(*(c->pos))) {
+			cur_token[i] = *(c->pos);
+			i++;
+			c->pos++;
+		}
+		c->pos++;
+		cur_token[i]=NULL;
+		return;
+	}
+
+	return; // TODO: replace this with an error, as we should never get here.
 }
 
 /* Returns the type of the current token */
 enum TOKEN_TYPE tokenType(codelist *c) {
+
+	// Check if we actually have no token left
+	if (cur_token[0] == 0) {
+		return T_NULL;
+	}
+
 	// Check for valid keyword
 	if (keyword())
 		return T_KEYWORD;
 
 	// Check if matches a symbol
-	for (int i=0;i<SYM_COUNT;i++) {
-		if (SYMBOLS[i]==cur_token[0])
-			return T_SYMBOL;
-	}
+	if (char_is_symbol(cur_token[0]))
+		return T_SYMBOL;
 
 	// Check if it starts with a number
-	if (SYMBOLS[0] >= "0" && SYMBOLS[0] <= "9")
+	if (char_is_int(cur_token[0]))
 		return T_INT_CONST;
 
 	// Check if STR_LITERAL flag set
